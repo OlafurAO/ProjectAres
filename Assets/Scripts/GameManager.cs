365b8e8cfbd93146c83c50a9c53c71732ec212f1
Ólafur <olafurao@ru.is>
2020-11-30 12:51:17 +0000

    e

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,14 +11,27 @@ public class GameManager : MonoBehaviour {
         instance = this;
     }
 
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
     void Update() {
         //TODO: move tmpCurrentUnit
-        //TODO: make tmpCurrentUnit attack another unit        
+        //TODO: make tmpCurrentUnit attack another unit  
+
+        // Left mouse click
+        if(Input.GetMouseButtonDown(0)) {
+            // Did player click on unit?
+            Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rhInfo;
+            bool didHit = Physics.Raycast(toMouse, out rhInfo, 500.0f);
+
+            if(didHit) {
+                print(rhInfo.collider.gameObject.name);
+            } else {
+                print("clicked on empty space");
+            }
+        }       
     }
 }
