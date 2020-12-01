c539a381803a61afc446f5dfd7caf8fccfe72afe
Ólafur <olafurao@ru.is>
2020-12-01 17:10:56 +0000

    e

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -20,6 +20,7 @@ public class GameManager : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
+        //TODO: Create units via code
         RollInitiative();
     }
 
